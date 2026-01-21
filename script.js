85cc42211a60925b33aa70b3cdc58c1a615cd541
williamgore <williamgore241@gmail.com>
2026-01-21 11:42:42 -0600

    Added mobile version

--- script.js
+++ script.js
@@ -187,22 +187,29 @@ function setResume() {
 
 }
 
-function moveButtons() {
-    const buttons = document.getElementById("sidebuttons")
-    
-    height = parseInt(buttons.offsetTop)
-    maxHeight = document.getElementById("main").offsetTop
-    console.log(maxHeight)
-    console.log(height)
+// Source - https://stackoverflow.com/a
+// Posted by Michael Zaporozhets, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-01-21, License - CC BY-SA 4.0
 
-    if(lastScrollPos - window.scrollY > 0) {
-        //height = buttons.style.top.substring(0,(buttons.style.top.length-2))
-       // buttons.style.top = (height+0.1) + "px"
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
 
-    } else {
-       // buttons.style.top = (height-0.1) + "px"
-    }
-    lastScrollPos = window.scrollY
+function setMobile() {
+
+    document.getElementById("sidebuttons").style.flexDirection = "row"
+    document.getElementById("sidebuttons").style.width = 100 + "vw"
+    document.getElementById("sidebuttons").style.justifyContent = "center"
+    document.getElementById("sidebuttons").style.top = 45 + "%"
+
+    document.getElementById("footer").style.paddingBottom = 90 + "px"
+
+    document.getElementById("title").style.marginLeft = 10 + "px"
+
+    document.getElementById("contentBox").style.marginLeft = 20 + "px"
+
 }
 
 function updateElement(newString, ID) {
@@ -253,10 +260,13 @@ function removeTitle(s, title, deleteTime) {
 
 ////////////////////////////////////////////////////////////
 
+//remove the !
+if(window.mobileCheck()) {
+    setMobile()
+}
+
+
 setHome()
-document.addEventListener("scroll", moveButtons);
-let lastScrollPos = 0
-console.log(lastScrollPos)
 
 let titles = ["a problem solver", "a developer", "a people person", "a dedicated learner", "the right choice."];
 updateTitles(titles)
